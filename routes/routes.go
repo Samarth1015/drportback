7060bec069a6a72784788bc2b54458b9be8426a1
samarth <202351125@iiitvadodara.ac.in>
2025-01-14 10:37:23 +0530

    checking add

--- routes/routes.go
+++ routes/routes.go
@@ -2,14 +2,19 @@ package routes
 
 import (
 	"back/controller"
-	
+	"encoding/json"
+	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
 func Route() *mux.Router {
 	route := mux.NewRouter()
-	
+	route.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Basic GET endpoint"))
+		json.NewEncoder(w).Encode("Hello");
+	}).Methods("GET");
 	route.HandleFunc("/api/mail",controller.SendData).Methods("POST")
 return route
 }
